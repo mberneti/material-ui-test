ee572712595c53555960464a62366cd33a026d09
mberneti <mberneti@live.com>
2020-10-28 17:07:26 +0330

    fixed some issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import { Button } from "@material-ui/core";
 import CustomButton1 from "./components/customButton1/CustomButton1";
@@ -8,7 +7,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>Hi Beauty! 🖐</p>
         <br />
         <Button disableElevation disableRipple variant="contained">
